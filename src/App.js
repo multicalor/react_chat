9ffaaf9eeeb6723e2963c70ba53fc3ca92680685
multicalor <d.tretiuk@gmail.com>
2020-09-12 11:57:35 +0300

    symple loging complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React,{ Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 // Component imports
-import Login from './components/Login/login'
-import Dashboard from './components/Dashboard/dashboard';
+import Login from "./components/Login/login";
+import Dashboard from "./components/Dashboard/dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <Fragment>
-        <Dashboard />
-      {/* <Login /> */}
-      </Fragment>
-    </div>
-  );
+
+
+// const loging = (user) => {
+//   if (user) {
+//     login = true;
+//     console.log("loging", login);
+//   }
+// };
+
+class App extends Component {
+
+  state = {
+    login: false,
+  }
+
+  loging = (name) => {
+    if (name){
+      this.setState({
+        login: true,
+      })
+    }
+    return
+  }
+
+  render() {
+    const { login } = this.state;
+
+    return (
+      <div className="App">
+        <Fragment>
+          {/* <Dashboard /> */}
+          {login ? <Dashboard /> : <Login loging={this.loging} />}
+        </Fragment>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,35 +2,61 @@ import React, { Component, Fragment } from 'react';
 import '../Login/style.css'
 
 class Login extends Component {
-    
+
     state = {
-        username: '',
+        userName: '',
         room: '',
+        // login: false,
     }
 
-    
+    handleInputChange = ({ target: { value } }) => {
+        this.setState({
+            userName: value,
+        })
+    }
+
+    handlerSelectRoomChange = ({ target: { value } }) => {
+        this.setState({
+            room: value,
+        })
+    }
+
+    handlerSubmitLogin = () => {
+        const { userName } = this.state;
+        const { loging } = this.props;
+        console.log(userName)
+        loging(userName)
+        
+    }
 
     render() {
+
+        const { userName, room } = this.state;
+        
+
+
         return (
-            <div class="join-container">
-                <header class="join-header">
-                    <h1><i class="fas fa-smile"></i> ChatCord</h1>
+            <div className="join-container">
+                <header className="join-header">
+                    <h1><i className="fas fa-smile"></i> ChatCord</h1>
                 </header>
-                <main class="join-main">
+                <main className="join-main">
                     <form action="chat.html">
-                        <div class="form-control">
+                        <div className="form-control">
                             <label for="username">Username</label>
                             <input
                                 type="text"
                                 name="username"
-                                id="username"
+                                // id="username"
                                 placeholder="Enter username..."
                                 required
+                                value={userName}
+                                onChange={this.handleInputChange}
                             />
                         </div>
-                        <div class="form-control">
+                        <div className="form-control">
                             <label for="room">Room</label>
-                            <select name="room" id="room">
+                            <select name="room" id="room" defaultValue={room} onChange={this.handlerSelectRoomChange}>
                                 <option value="JavaScript">JavaScript</option>
                                 <option value="Python">Python</option>
                                 <option value="PHP">PHP</option>
@@ -39,7 +65,7 @@ class Login extends Component {
                                 <option value="Java">Java</option>
                             </select>
                         </div>
-                        <button type="submit" class="btn">Join Chat</button>
+                        <button type="submit" class="btn" onClick={this.handlerSubmitLogin}>Join Chat</button>
                     </form>
                 </main>
             </div>
